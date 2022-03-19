1e7b5b304be446992258b2eb5d07520a4f32cbda
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-19 15:20:23 -0700

    nav links hooked up

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './Home';
+import { Routes, Route } from 'react-router-dom';
+import Cuisine from './Cuisine';
 
 function Pages() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Cuisine />} />
+      </Routes>
     </div>
   );
 }
